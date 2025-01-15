10cc537a3fb372ea6b80ef1b050be818dfe0f699
Steven Webster <swebsterdev@gmail.com>
2025-01-15 16:56:48 +0800

    feat: override bootstrap variables, add custom scss

--- tablets-scaling/frontend/src/main.tsx
+++ tablets-scaling/frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './App.scss';
+import './scss/App.scss';
 import { App } from './App';
 
 const rootDiv = document.getElementById('root');
