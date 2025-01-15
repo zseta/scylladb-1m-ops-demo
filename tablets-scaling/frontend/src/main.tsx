b9b66e24b9a17fb2967bd152ca4ea982663ae92b
Steven Webster <swebsterdev@gmail.com>
2025-01-15 16:25:55 +0800

    chore: bring in App.jsx, import bootstrap SASS, remove boilerplate

--- tablets-scaling/frontend/src/main.tsx
+++ tablets-scaling/frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import './App.scss';
+import { App } from './App';
 
 const rootDiv = document.getElementById('root');
 
