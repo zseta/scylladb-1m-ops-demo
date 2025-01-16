db3ed791bdd98dfc829a7bca18d91a371a275525
Steven Webster <swebsterdev@gmail.com>
2025-01-16 22:34:15 +0800

    refactor: tidy up <TabsLayout /> component

--- tablets-scaling/frontend/src/components/TabsLayout.tsx
+++ tablets-scaling/frontend/src/components/TabsLayout.tsx
@@ -5,43 +5,54 @@ import { Icon } from '@/components/Icon';
 import { Scenarios } from '@/components/Scenarios';
 import { About } from '@/components/About';
 
+type Key = 'dashboard' | 'scenarios' | 'about';
+
+interface SectionTab {
+  readonly key: Key;
+  readonly title: string;
+  readonly iconVariant: string;
+  readonly component: () => ReactElement;
+}
+
+const tabs: readonly SectionTab[] = [
+  {
+    key: 'dashboard',
+    title: 'Dashboard',
+    iconVariant: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    key: 'scenarios',
+    title: 'Scenarios',
+    iconVariant: 'rocket',
+    component: Scenarios,
+  },
+  {
+    key: 'about',
+    title: 'About',
+    iconVariant: 'info',
+    component: About,
+  },
+];
+
 export const TabsLayout = (): ReactElement => (
   <Tabs
-    defaultActiveKey="home"
+    defaultActiveKey="dashboard"
     id="controlTabs"
     className="nav-tabs nav-fill"
   >
-    <Tab
-      eventKey="home"
-      title={
-        <>
-          <Icon variant="dashboard" /> Dashboard
-        </>
-      }
-    >
-      <Dashboard />
-    </Tab>
-
-    <Tab
-      eventKey="settings"
-      title={
-        <>
-          <Icon variant="rocket" /> Scenarios
-        </>
-      }
-    >
-      <Scenarios />
-    </Tab>
-
-    <Tab
-      eventKey="about"
-      title={
-        <>
-          <Icon variant="info" /> About
-        </>
-      }
-    >
-      <About />
-    </Tab>
+    {tabs.map(({ key, title, iconVariant, component: Component }) => (
+      <Tab
+        key={key}
+        eventKey={key}
+        title={
+          <>
+            <Icon variant={iconVariant} /> {title}
+          </>
+        }
+      >
+        <Component />
+      </Tab>
+    ))}
   </Tabs>
 );
