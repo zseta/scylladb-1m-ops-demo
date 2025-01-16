d57c16e31c261772a48b42266dbe4d914d50328a
Steven Webster <swebsterdev@gmail.com>
2025-01-16 18:01:37 +0800

    refactor: remove About component to module

--- tablets-scaling/frontend/src/App.tsx
+++ tablets-scaling/frontend/src/App.tsx
@@ -3,10 +3,10 @@ import { Tabs, Tab, Button, Collapse, Card, Spinner } from 'react-bootstrap';
 import { Icon } from '@/components/Icon';
 import { GrafanaContainer } from '@/components/Grafana';
 import { Dashboard } from '@/components/Dashboard';
+import { About } from '@/components/About';
 import { useSocketContext } from '@/context/socket';
 import { SocketProvider } from '@/context/SocketProvider';
 import logo from '@/assets/images/scylla-logo.svg';
-import mascot from '@/assets/images/scylladb-mascot-cloud.svg';
 
 export const App = () => (
   <SocketProvider>
@@ -232,70 +232,7 @@ const MainContainer = (): ReactElement => {
             </>
           }
         >
-          <>
-            <Card>
-              <Card.Body className="section-about">
-                <img
-                  src={mascot}
-                  alt=""
-                />
-                <h2>
-                  1 million ops/sec <br />
-                  ScyllaDB demos with Terraform
-                </h2>
-                <p className="lead">
-                  Test and benchmark ScyllaDB under a 1 million operations per
-                  second workload.
-                </p>
-              </Card.Body>
-            </Card>
-
-            <div className="flex-grow-1">
-              <div className="hstack justify-content-center gap-3 flex-wrap">
-                <Button
-                  variant="light"
-                  href="https://www.scylladb.com"
-                  target="_blank"
-                >
-                  ScyllaDB.com
-                </Button>
-                <Button
-                  variant="light"
-                  href="https://docs.scylladb.com"
-                  target="_blank"
-                >
-                  <Icon icon="docs" />
-                  Documentation
-                </Button>
-                <Button
-                  variant="light"
-                  href="https://github.com/scylladb"
-                  target="_blank"
-                >
-                  <Icon icon="github" />
-                  GitHub
-                </Button>
-                <Button
-                  variant="light"
-                  href="https://twitter.com/scylladb"
-                  target="_blank"
-                >
-                  <Icon icon="x" />X
-                </Button>
-                <Button
-                  variant="light"
-                  href="https://www.linkedin.com/company/scylladb"
-                  target="_blank"
-                >
-                  <Icon icon="linkedin" />
-                  LinkedIn
-                </Button>
-              </div>
-            </div>
-            <div className="border-top pt-3 small text-center">
-              &copy; {new Date().getFullYear()} ScyllaDB. All rights reserved.
-            </div>
-          </>
+          <About />
         </Tab>
       </Tabs>
     </div>
---
+++ tablets-scaling/frontend/src/components/About/index.tsx
@@ -0,0 +1,71 @@
+import type { ReactElement } from 'react';
+import { Icon } from '@/components/Icon';
+import { Button, Card } from 'react-bootstrap';
+import mascot from '@/assets/images/scylladb-mascot-cloud.svg';
+
+export const About = (): ReactElement => (
+  <>
+    <Card>
+      <Card.Body className="section-about">
+        <img
+          src={mascot}
+          alt=""
+        />
+        <h2>
+          1 million ops/sec <br />
+          ScyllaDB demos with Terraform
+        </h2>
+        <p className="lead">
+          Test and benchmark ScyllaDB under a 1 million operations per second
+          workload.
+        </p>
+      </Card.Body>
+    </Card>
+
+    <div className="flex-grow-1">
+      <div className="hstack justify-content-center gap-3 flex-wrap">
+        <Button
+          variant="light"
+          href="https://www.scylladb.com"
+          target="_blank"
+        >
+          ScyllaDB.com
+        </Button>
+        <Button
+          variant="light"
+          href="https://docs.scylladb.com"
+          target="_blank"
+        >
+          <Icon icon="docs" />
+          Documentation
+        </Button>
+        <Button
+          variant="light"
+          href="https://github.com/scylladb"
+          target="_blank"
+        >
+          <Icon icon="github" />
+          GitHub
+        </Button>
+        <Button
+          variant="light"
+          href="https://twitter.com/scylladb"
+          target="_blank"
+        >
+          <Icon icon="x" />X
+        </Button>
+        <Button
+          variant="light"
+          href="https://www.linkedin.com/company/scylladb"
+          target="_blank"
+        >
+          <Icon icon="linkedin" />
+          LinkedIn
+        </Button>
+      </div>
+    </div>
+    <div className="border-top pt-3 small text-center">
+      &copy; {new Date().getFullYear()} ScyllaDB. All rights reserved.
+    </div>
+  </>
+);
